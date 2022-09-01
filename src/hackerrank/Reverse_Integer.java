d647aaf6ba6d19dbda57cb17bdd54777b8f0a164
Stanislav Shevchenko <stas1234713@gmail.com>
2022-09-01 21:39:51 +0300

    +Reverse_Integer

---
+++ src/hackerrank/Reverse_Integer.java
@@ -0,0 +1,17 @@
+package hackerrank;
+
+public class Reverse_Integer {
+    public int reverse(int x) {
+        long res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+
+        if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) {
+            return 0;
+        } else {
+            return (int)res;
+        }
+    }
+}
