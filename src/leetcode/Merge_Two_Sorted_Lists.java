ea9f9cfeac4b767219f4daf0cb6be39e5bf8eaa6
Stanislav Shevchenko <stas1234713@gmail.com>
2022-09-04 20:24:42 +0300

    +Climbing_Stairs

--- src/leetcode/Merge_Two_Sorted_Lists.java
+++ src/leetcode/Merge_Two_Sorted_Lists.java
@@ -5,20 +5,20 @@ import java.util.stream.IntStream;
 
 public class Merge_Two_Sorted_Lists {
     public static void main(String[] args) {
-        int[]arr1 = {1,2,4};
-        int[]arr2 = {1,3,4};
+        int[]arr1 = {1,2,4,5};
+        int[]arr2 = {1,3,4,5,6};
         merge(arr1, arr2);
     }
 //its not a solved, its a random cod that I write for task number 21.
     public static void merge(int[]arr1, int[]arr2){
         int[] arr3 = new int[arr1.length+arr2.length];
 
-        for (int i=0; i< arr3.length/2;i++){
+        for (int i=0; i<arr3.length- arr1.length-1;i++){
             arr3[i]=arr1[i];
         }
         int k=0;
-        for (int i= arr3.length/2; i< arr3.length;i++){
-            arr3[i]=arr2[0];
+        for (int i= arr3.length- arr2.length; i<=arr3.length;i++){
+            arr3[i]=arr2[k];
             k++;
         }
 
