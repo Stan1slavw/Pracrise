aae6786dc0a3cb364dbfa5eee757753c7b5eb1db
Stanislav Shevchenko <stas1234713@gmail.com>
2022-09-16 20:20:08 +0300

    +Valid_Parentheses

---
+++ src/codewars/Valid_Parentheses.java
@@ -0,0 +1,14 @@
+package codewars;
+
+public class Valid_Parentheses {
+    public static boolean validParentheses(String parens) {
+        int count = 0;
+
+        for (int i=0; i<parens.length(); i++) {
+            if (parens.charAt(i)=='(') count ++;
+            else if (parens.charAt(i)==')') count--;
+            if (count<0) return false;
+        }
+        return count==0;
+    }
+}
