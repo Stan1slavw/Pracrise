168ad436b43a3dc6727f2af3d43652d130135b4a
Stanislav Shevchenko <stas1234713@gmail.com>
2022-09-17 18:03:44 +0300

    +Bouncing_Balls

---
+++ src/codewars/Bouncing_Balls.java
@@ -0,0 +1,10 @@
+package codewars;
+
+public class Bouncing_Balls {
+    public static int bouncingBall(double h, double bounce, double window) {
+        if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) {
+            return -1;
+        }
+        return 2 + bouncingBall(h * bounce, bounce, window);
+    }
+}
