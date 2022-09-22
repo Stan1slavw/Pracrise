b4fa249ae41f7c4867f1af4f59a3560f656cbf93
Stanislav Shevchenko <stas1234713@gmail.com>
2022-09-22 21:24:01 +0300

    +String_ends_with

---
+++ src/codewars/String_ends_with.java
@@ -0,0 +1,11 @@
+package codewars;
+
+public class String_ends_with {
+    public static boolean solution(String str, String ending) {
+        if (str.endsWith(ending)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
